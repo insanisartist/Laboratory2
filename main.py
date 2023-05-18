ed99d4e9f0b96d36179d6ed09fae6a446f0c426d
Insanis <78218644+insanisartist@users.noreply.github.com>
2023-05-18 12:35:46 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,23 @@ from aiogram.utils import executor
 import parser
 bot = Bot(token="6287055552:AAGf6FM3-z3prT0NWwHFLF7VgEEH-8hEBF4")
 dp = Dispatcher(bot)
+def apteki(strmsg):
+    url = (strmsg)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html.parser")
+    soup.get_text(strip=True)
+    mydivs = soup.find_all("div", {"class": "m-result-item item-single-price"})
+    k = 0
+    result = ""
+    for data in mydivs:
+        if (k<5):
+            text = data.text.strip()
+            text = text.replace("\n", " ")
+            text = text.replace("      ", " ")
+            text = text.replace("\r", " ")
+            result += text + "\n"
+            k+=1
+    return result
 @dp.message_handler(commands=["start"])
 async def cmd_start(msg: types.Message) -> None:
     await msg.answer("Добрый день, для поиска лекарств введите комманду /search")
@@ -18,25 +35,24 @@ async def cmd_start(msg: types.Message) -> None:
         @dp.message_handler()
         async def cmd_search(msg: types.Message) -> None:
             strmsg= str(msg.text)
-            d = dict()
-            k = int()
-            text = str()
-            d = str()
-            d = strmsg
-            url = 'https://omsk.vapteke.ru/search?s=' + (d)
+            url = 'https://omsk.vapteke.ru/search?s=' + (strmsg)
             page = requests.get(url)
             soup = BeautifulSoup(page.text, "html.parser")
             soup.get_text(strip=True)
             mydivs = soup.find_all("a", {"class": "media-heading init-loader"})
-            mydivs2 = soup.find_all("div", {"class": "m-result-price flex-wide"})
-            result = str()
+            mydivs2 = soup.find_all("div", {"class": "media-body"})
+            result = ""
             for data in mydivs:
+                ssilka = ""
                 item_url = str(data.get("href"))
                 text = data.text.strip()
                 text = text.replace("\n", " ")
+                text = text.replace("      ", " ")
                 text = text.replace("\r", " ")
-                result+= ("\n" + "🟢 " + text + "\n" +"Ссылка на страницу с аптеками где есть данный товар" + " " + item_url + "\n" )
-            await msg.answer(result)
+                text = text.replace("Подробнее", " ")
+                ssilka = apteki(item_url)
+                result= ("\n" + "🟢 " + text + "\n" + " " +  "\n"+ ssilka )
+                await msg.answer(result)
 
 
 if __name__ == "__main__":
